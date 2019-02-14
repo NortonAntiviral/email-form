b5851bbac76677f0ce74f7a225f77a113227476c
NortonAntiviral <officialantiviral@gmail.com>
2019-02-13 23:52:00 -0500

    added css and validation for forms

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
 // Variables
-const sendBtn = document.getElementById('sendBtn');
+const sendBtn = document.getElementById('sendBtn'),
+    email = document.getElementById('email'),
+    subject = document.getElementById('subject'),
+    message = document.getElementById('message');
 
 // Event Listeners
 
@@ -8,6 +11,11 @@ eventListeners();
 function eventListeners() {
     //App init
     document.addEventListener('DOMContentLoaded', appInit);
+
+    // Validate the forms
+    email.addEventListener('blur', validateField);
+    subject.addEventListener('blur', validateField);
+    message.addEventListener('blur', validateField);
 }
 
 // Functions
@@ -17,4 +25,43 @@ function appInit() {
     // disable send button on load
     sendBtn.disabled = true;
 
+}
+
+// Validate the fields
+function validateField() {
+    let errors;
+
+    //Validate the lenth of the field
+    validateLength(this);
+
+    // Validate the email
+    // console.log(this);
+    if(this.type === 'email') {
+        validateEmail(this);
+    }
+}
+
+// Validate the lenth of the fields
+function validateLength(field) {
+    if(field.value.length > 0) {
+        field.style.borderBottomColor = 'green';
+        field.classList.remove('error');
+    } else {
+        field.style.borderBottomColor = 'red';
+        field.classList.add('error');
+    }
+}
+
+// Validate email checks @ symbol
+function validateEmail(field) {
+    let emailText = field.value;
+    // checks for @ sign
+    if(emailTextindexOf('@' && '.') !== -1) {
+        field.style.borderBottomColor = 'green';
+        field.classList.remove('error');
+    } else {
+        field.style.borderBottomColor = 'red';
+        field.classList.add('error');
+    
+    }
 }
